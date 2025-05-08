4f59021ad967aa8a5c84f81d393f3e9dcb4b37b4
1wldms <jieunin0311@gmail.com>
2025-05-08 22:44:16 +0900

    j

---
+++ templates/login.js
@@ -0,0 +1,30 @@
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+}
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const button = document.getElementById("login-btn");
+const form = document.getElementById("login-form");
+
+function checkInputs() {
+    const usernameFilled = username.value.trim() !== "";
+    const passwordFilled = password.value.trim() !== "";
+
+    if (usernameFilled && passwordFilled) {
+        button.disabled = false;
+        button.classList.add("active");
+    } else {
+        button.disabled = true;
+        button.classList.remove("active");
+    }
+}
+
+username.addEventListener("input", checkInputs);
+password.addEventListener("input", checkInputs);
+
+form.addEventListener("submit", function (e) {
+    //e.preventDefault(); // prevent actual submission
+    //alert("Logged in successfully! 🔐");
+});
